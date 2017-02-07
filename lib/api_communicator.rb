7d848ce5a9ec373cda11f09d84b640e5ef21b14a
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-07 17:50:59 -0500

    finished

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -16,15 +16,83 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+  
+  films = []
+  character_hash["results"].each do |char|
+    if char["name"] == character
+      films = char["films"]
+    end
+  end
+
+  get_hashes_from_urls(films)
+  film_array = []
+  films.each do |film|
+    film_client = RestClient.get(film)
+    film_array << JSON.parse(film_client)
+  end
+
+  film_array
+
 end
+
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  film_titles = []
+  films_hash.each do |film|
+    film_titles << film["title"]
+  end
+  puts film_titles
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
+end
+
+def get_movie_info_from_api(movie_name, category)
+  film_rest_client = RestClient.get('www.swapi.co/api/films/')
+  all_films = JSON.parse(film_rest_client)
+
+  all_films["results"].each do |film|
+    if film["title"] == movie_name
+      return film[category]
+    end
+  end
+end
+
+def array_check?(category_array)
+  category_array.class == Array
+end
+
+def show_movie_info(movie_name, category)
+  movie_info = get_movie_info_from_api(movie_name, category)
+  
+  if array_check?(movie_info)
+    category_array_of_hashes = get_hashes_from_urls(movie_info)
+    parse_category_info(category_array_of_hashes)
+  else
+    puts movie_info
+  end
+end
+
+def get_hashes_from_urls(array)
+  new_array = []
+  array.each do |category|
+    category_client = RestClient.get(category)
+    new_array << JSON.parse(category_client)
+  end
+  new_array
+end
+
+def parse_category_info(array)
+  category_names = []
+
+  array.each do |category|
+
+    category_names << category["name"]
+  end
+  p category_names
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,57 @@
+require_relative "api_communicator.rb"
+
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to the Star Wars database!"
+
+end
+
+def check_valid_answer(string)
+	until string == "yes" || string == "no"
+		puts "Please say 'yes' or 'no'"
+		string = gets.strip
+	end
+	string
+end
+
+def either_or
+	puts "Would you like to look up what movies your favorite Star Wars character was in?"
+	character_answer = gets.strip
+	character_answer = check_valid_answer(character_answer)
+
+	if character_answer == "yes"
+		character = get_character_from_user
+		show_character_movies(character)
+	end
+
+	puts "Would you like to look up other information about a specific movie?"
+	movie_answer = gets.strip
+	movie_answer = check_valid_answer(movie_answer)
+
+	if movie_answer == "yes"
+		movie_name = get_movie_name_from_user
+		category_name = get_category_name_from_user
+		show_movie_info(movie_name, category_name)
+	end
+
+
+
 end
 
 def get_character_from_user
   puts "please enter a character"
+  character_name = gets.strip
+  character_name
   # use gets to capture the user's input. This method should return that input, downcased.
+end
+
+def get_movie_name_from_user
+	puts "Please enter a Movie Name"
+	movie_name = gets.strip
+end
+
+def get_category_name_from_user
+	puts "Please enter a Category"
+	category_name = gets.strip
 end
